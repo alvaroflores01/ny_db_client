ab91df879459f0d0aadf852b86502b8a76ad8c6a
alvaroflores <alvaro@brolty.com>
2024-03-27 16:40:19 -0400

    added axios example

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,3 +1,39 @@
-export default function Profile() {
-  return <h1>Profile</h1>;
+'use client';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+function Profile() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/testTable');
+        // Assuming you have an API route set up
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Example</h1>
+      {data ? (
+        <ul>
+          <h1>Results:</h1>
+          {data.map((item) => (
+            <li key={item[0]}>{`${item[0]}: ${item[1]}`}</li>
+          ))}{' '}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
+
+export default Profile;
